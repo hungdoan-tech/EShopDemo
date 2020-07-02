1e598638cc6348088abb2a7b83050132f59ba6e7
hungdoan7 <hungdoan426@gmail.com>
2020-07-02 22:35:30 +0700

    AddingAlmostLayout

---
+++ Spice/wwwroot/js/OrderTracking.js
@@ -0,0 +1,22 @@
+﻿const requestTrackingOrdering = async () => {
+    try {
+        const countRaw = await fetch('/Customer/Order/TrackingOrder')
+
+        if (countRaw) {
+            const count = await countRaw.json()
+        }
+
+        if (count) {
+            if (count === 0) {
+                document.getElementById('trackingOrdering').innerHTML = 'Tracking Ordering'
+            }
+            else {
+                document.getElementById('trackingOrdering').innerHTML = 'Tracking Ordering (' + count + ')'
+            }
+        }
+    }
+    catch (error) {
+        console.log(error)
+    }
+}
+requestTrackingOrdering()
