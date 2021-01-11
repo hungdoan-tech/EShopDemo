eb9890bd27820f6547504194a2437616b3f823e5
ngTruc <nguyentruc11099@gmail.com>
2021-01-11 18:45:20 +0700

    Test Check Item Quantity Part 1

--- Spice/Service/ServiceInterfaces/ISessionService.cs
+++ Spice/Service/ServiceInterfaces/ISessionService.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Spice.Models.ViewModels;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -9,5 +10,6 @@ namespace Spice.Service.ServiceInterfaces
     {
         void Clear();
         object GetSession(string code);
+        List<MenuItemsAndQuantity> GetSessionListQuantity();
     }
 }
